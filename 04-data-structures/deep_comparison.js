4ad36b4d235bb5b5c3602c9be992a4d53be6dbfc
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-02-10 21:57:44 -0700

    modify deep_comparison

--- 04-data-structures/deep_comparison.js
+++ 04-data-structures/deep_comparison.js
@@ -1,1 +1,12 @@
-const deppEqual = (val1, val2) => {};
+const deepEqual = (val1, val2) => {
+  if (val1 === null || val2 === null) return false;
+  else if (typeof val1 !== 'object' || typeof val2 !== 'object')
+    return val1 === val2;
+  else if (Object.keys(val1).length !== Object.keys(val2).length) return false;
+  else return deepEqual(Object.keys(val1), Object.keys(val2));
+};
+
+let obj = { here: { is: 'an' }, object: 2 };
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+console.log(deepEqual(obj, { here: { is: 'an' }, object: 2 }));
